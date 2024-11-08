e626f33be48b78a8d7d5c8037d1fa3b9a85a77d2
Chandraj N. Hejmady <chandrajhejmady@gmail.com>
2024-11-08 11:08:47 +0530

    App's View Model

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -1,8 +1,13 @@
 package com.plcoding.cryptotracker.crypto.data.networking
 
+import com.plcoding.cryptotracker.core.data.networking.constructUrl
+import com.plcoding.cryptotracker.core.data.networking.responseToResult
 import com.plcoding.cryptotracker.core.data.networking.safeCall
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
+import com.plcoding.cryptotracker.core.domain.util.map
+import com.plcoding.cryptotracker.crypto.data.mappers.toCoin
+import com.plcoding.cryptotracker.crypto.data.networking.dto.CoinsResponseDto
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
 import io.ktor.client.HttpClient
@@ -12,8 +17,10 @@ class RemoteCoinDataSource (
     private val httpClient: HttpClient
 ): CoinDataSource{
     override suspend fun getCoins(): Result<List<Coin>, NetworkError> {
-        return safeCall {
-            httpClient.get()
-        }
+        return safeCall<CoinsResponseDto> {
+            httpClient.get(
+                urlString = constructUrl("/assets")
+            )
+        }.map { response -> response.data.map { it.toCoin() } }
     }
 }
